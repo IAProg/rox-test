8172bd0150d8571c749a7b7881c4ba74392dfb52
Alex Heppell <alexheppell@protonmail.com>
2022-11-12 23:31:20 +0000

    add colour disc component

---
+++ src/components/game-board/colour-disc.ts
@@ -0,0 +1,21 @@
+import { Container, Sprite, Texture } from "pixi.js";
+import { gameConfig } from "../../config";
+
+export class ColourDisc extends Container {
+    private _disc: Sprite;
+    
+    constructor(){
+        super();
+        this._disc = new Sprite(Texture.WHITE);
+        this._disc.anchor.set(0.50, 0.00);
+        
+        const discSize = gameConfig.disc.size;
+        this._disc.scale.set(discSize / this._disc.width);
+        this._disc.tint = gameConfig.colours[0];
+        this.addChild(this._disc);
+    }
+
+    public async moveTo(): Promise<void>{
+        //
+    }
+}
