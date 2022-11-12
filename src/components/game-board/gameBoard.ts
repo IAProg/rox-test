c6bffe5f92299b00b205398900dc0457964738e9
Alex Heppell <alexheppell@protonmail.com>
2022-11-12 23:31:49 +0000

    add components to gameboard

--- src/components/game-board/gameBoard.ts
+++ src/components/game-board/gameBoard.ts
@@ -1,7 +1,43 @@
 import { Container } from "pixi.js";
+import { gameConfig } from "../../config";
+import { ColourDisc } from "./colour-disc";
+import { ColourSelector } from "./colour-selector";
+import { TextFields } from "./text-fields";
 
 export class Gameboard extends Container {
+    private _colourSelector: ColourSelector;
+    private _colourDisc: ColourDisc;
+    private _textFields: TextFields;
+
+    private size: {
+        width: number,
+        height: number
+    };
+    
     constructor(){
         super();
+        this._colourSelector = new ColourSelector();
+        this._colourDisc = new ColourDisc();
+        this._textFields = new TextFields();
+
+        const { selectorPadding, textFieldPadding, padding } = gameConfig.gameboard;
+        this._colourDisc.y     = this._textFields.getBounds().bottom + (this._textFields.height * textFieldPadding);
+        this._colourSelector.y = this._colourDisc.getBounds().bottom + (this._colourDisc.height * selectorPadding);
+
+        this.addChild(this._colourSelector, this._colourDisc, this._textFields);
+
+        this.size = {
+            width:  padding * this.width,
+            height: padding * this.height
+        }
+    }
+
+    public resize(width: number, height: number): void{
+        this.scale.set(Math.min(
+            width  / this.size.width,
+            height / this.size.height
+        ));
+
+        this.position.x = width * 0.50;
     }
 }
