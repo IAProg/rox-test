d31308fd65c0610e28b66c0fda7388f385697498
Alex Heppell <alexheppell@protonmail.com>
2022-11-14 23:02:00 +0000

    fix stack limit error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
-
+import gsap from "gsap";
 
 export async function asyncTween (targets: gsap.TweenTarget, vars: gsap.TweenVars): Promise<void>{
     return new Promise(resolve => {
         vars.onComplete = () => {
-            vars.onComplete && vars.onComplete();
+            vars.onComplete && (() => vars.onComplete());
             resolve();
         }
         gsap.to(targets, vars)
