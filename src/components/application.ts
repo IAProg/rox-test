d4ac1a9e36d9a734275b8c4e796dd3bd47d91c53
Alex Heppell <alexheppell@protonmail.com>
2022-11-14 23:10:28 +0000

    add result board

--- src/components/application.ts
+++ src/components/application.ts
@@ -1,6 +1,6 @@
-import { Back } from "gsap";
 import { Application } from "pixi.js";
 import { gameConfig } from "../config";
+import { delay } from "../utils";
 import { Background } from "./background";
 import { Gameboard } from "./game-board";
 import { ResultBoard } from "./result-board";
@@ -14,14 +14,20 @@ export class RoxApp extends Application {
         super(gameConfig.canvas)
         this._background = new Background();
         this._gameboard = new Gameboard();
+        this._resultBoard = new ResultBoard();
 
-        this.stage.addChild(this._background, this._gameboard);
+        this.stage.addChild(this._background, this._gameboard, this._resultBoard);
 
         this.scaleContent(gameConfig.canvas.width, gameConfig.canvas.height);
+
+        this.onBegin();
     }
 
     public async onBegin(): Promise<void>{
         //
+
+        await delay(1000);
+        this._resultBoard.show(10);
     }
 
     public async onEnd(): Promise<void>{
@@ -31,5 +37,6 @@ export class RoxApp extends Application {
     public scaleContent(width: number, height: number): void{
         this._gameboard.resize(width, height);
         this._background.resize(width, height);
+        this._resultBoard.resize(width, height);
     }
 }
--- src/components/result-board/resultBoard.ts
+++ src/components/result-board/resultBoard.ts
@@ -1,7 +1,66 @@
-import { Container } from "pixi.js";
+import { Container, Sprite, Text } from "pixi.js";
+import { getTexture } from "../../asset-loader";
+import { gameConfig, strings } from "../../config";
+import { asyncTween } from "../../utils";
 
 export class ResultBoard extends Container {
+    private _backdrop: Sprite;
+
+    private _message: Text;
+    private _amount: Text;
+
+    private size: {
+        width: number,
+        height: number
+    };
+    
     constructor(){
         super();
+        this._backdrop = new Sprite(getTexture("gameboard"));
+        this._backdrop.anchor.set(0.50);
+        
+        const { padding, style, amountPos, messagePos } = gameConfig.results;
+
+        this._message = new Text(strings.results.message, style);
+        this._message.anchor.set(0.5, 0.00);
+        this._message.position.set(messagePos.x, messagePos.y);
+        
+        this._amount = new Text("PLACEHOLDER", style);
+        this._amount.anchor.set(0.5, 0.00);
+        this._amount.position.set(amountPos.x, amountPos.y);
+
+        this.addChild(this._backdrop, this._message, this._amount);
+
+        this.size = {
+            width:  padding * this.width,
+            height: padding * this.height
+        }
+
+        this.interactive = true;
+        this.alpha = 0;
+    }
+
+    public async show(score: number): Promise<void>{
+        this._amount.text = String(score);
+        const { fadeDuration } = gameConfig.results;
+        await asyncTween(this, { duration: fadeDuration, alpha: 1 });
+        await this.awaitPress();
+        await asyncTween(this, { duration: fadeDuration, alpha: 0 });
+    }
+
+    public resize(width: number, height: number): void{
+        this.scale.set(Math.min(
+            width  / this.size.width,
+            height / this.size.height
+        ));
+
+        this.position.set(
+            width * 0.50,
+            height * 0.50
+        )
+    }
+
+    private awaitPress(): Promise<void>{
+        return new Promise((resolve) => this.on("pointerdown", resolve))
     }
 }
