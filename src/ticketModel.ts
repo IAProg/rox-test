40f22d22cafca9eb571a51c6786d262e18098085
Alex Heppell <alexheppell@protonmail.com>
2022-11-15 00:13:00 +0000

    add ticket model and response type

---
+++ src/ticketModel.ts
@@ -0,0 +1,18 @@
+import { IResponseData } from "./types";
+
+class TicketModel{
+
+    private _colours: Array<number>;
+    private _colourIndex: number;
+
+    public get nextColour(): number{
+        const colour = this._colours[this._colourIndex];
+        this._colourIndex++;
+        return colour;
+    }
+
+    public setData(responseData: IResponseData): void{
+        this._colours = responseData.colours;
+        this._colourIndex = 0;
+    }
+}
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,7 @@ export interface ITextureAsset {
     name: string;
     url: string;
 }
+
+export interface IResponseData {
+    colours: Array<number>;
+}
