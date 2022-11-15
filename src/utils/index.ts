de7283a16b500bb7363c0398b992cb77bfd0e3f5
Alex Heppell <alexheppell@protonmail.com>
2022-11-15 19:38:21 +0000

    add random generator util methods

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,3 +13,15 @@ export async function asyncTween (targets: gsap.TweenTarget, vars: gsap.TweenVar
 export const delay = (ms: number) => new Promise<void>(
     (resolve) => setTimeout(resolve, ms)
 );
+
+export function randomFloat(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return (Math.random() * (max - min)) + min;
+}
+
+export function randomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
