42691b40d380c0e6318b89ab68330a38b205cf18
Alex Heppell <alexheppell@protonmail.com>
2022-11-15 23:13:20 +0000

    complete gameplay loop

--- src/components/application.ts
+++ src/components/application.ts
@@ -1,5 +1,7 @@
 import { Application } from "pixi.js";
 import { gameConfig } from "../config";
+import { requestData } from "../requests";
+import { ticketModel } from "../ticketModel";
 import { delay } from "../utils";
 import { Background } from "./background";
 import { Gameboard } from "./game-board";
@@ -9,6 +11,7 @@ export class RoxApp extends Application {
     private _background: Background;
     private _gameboard: Gameboard;
     private _resultBoard: ResultBoard;
+    private _inPlay: boolean = true;
 
     constructor(){
         super(gameConfig.canvas)
@@ -29,12 +32,27 @@ export class RoxApp extends Application {
         this._resultBoard.resize(width, height);
     }
 
+    // the main gameloop
     private async play(): Promise<void>{
-        this._gameboard.preconfigure();
-        const score = await this._gameboard.play();
-        await delay(2000); // wait a moment before showing the score
-        await this._resultBoard.show(score);
+        while( this._inPlay ){
+            //setup
+            ticketModel.setData(await requestData());
+            this._gameboard.preconfigure();
 
-        this.play();
+            //play
+            await this._gameboard.setFade(true);
+            const score = await this._gameboard.play();
+            this._resultBoard.updateValue(score)
+
+            await delay(2000); // wait a moment before showing the score
+
+            await Promise.all([ // bring the results into view
+                this._gameboard.setFade(false),
+                this._resultBoard.setFade(true)
+            ]);
+
+            await this._resultBoard.awaitPress();
+            await this._resultBoard.setFade(false);
+        }
     }
 }
