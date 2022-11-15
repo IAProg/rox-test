782f78389b848f49fd0d2e9f90d348d826d07113
Alex Heppell <alexheppell@protonmail.com>
2022-11-15 00:03:39 +0000

    selector now locks on press

--- src/components/game-board/colour-selector.ts
+++ src/components/game-board/colour-selector.ts
@@ -2,6 +2,8 @@ import { Container, Sprite, Texture } from "pixi.js";
 import { gameConfig } from "../../config";
 
 export class ColourSelector extends Container {
+    public selection: number;
+
     private _selectionContainer: Container;
     private _colourOptions: Sprite[];
     
@@ -18,7 +20,6 @@ export class ColourSelector extends Container {
             button.tint = colour;
 
             button.on("pointerdown", () => this.onSelect(index), this);
-            button.interactive = true;
             return button;
         });
 
@@ -30,12 +31,24 @@ export class ColourSelector extends Container {
     }
 
     public async awaitSelection(): Promise<number>{
+        this.setEnabled(true);
         return new Promise<number>((resolve) => 
             this.on("selectionMade", (selction) => resolve(selction))
         );
     }
 
     private onSelect(index: number): void{
+        this.setEnabled(false);
+        this.selection = index;
         this.emit("selectionMade", index);
+    }
+
+    private setEnabled(enabled: boolean): void{
+        const alpha = enabled ? 1.00 : 0.50;
+        this._colourOptions.forEach(option => {
+            option.interactive = enabled;
+            option.alpha = alpha;
+        });
+
     }
 }
