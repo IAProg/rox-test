b76d90b9a399ae115d6750bbf88e24992bf66a69
Alex Heppell <alexheppell@protonmail.com>
2022-11-15 22:56:20 +0000

    asset upgrade

--- src/asset-loader.ts
+++ src/asset-loader.ts
@@ -4,7 +4,13 @@ import { ITextureAsset } from "./types";
 const textureList = [
     { name: "background", url: "background.png"},
     { name: "pointer", url: "pointer.png"},
-    { name: "gameboard", url: "gameboard.png"}
+    { name: "btn0", url: "btn0.png"},
+    { name: "btn1", url: "btn1.png"},
+    { name: "btn2", url: "btn2.png"},
+    { name: "btn3", url: "btn3.png"},
+    { name: "btn4", url: "btn4.png"},
+    { name: "gameboard", url: "gameboard.png"},
+    { name: "resultboard", url: "resultsboard.png"}
 ] as Array<ITextureAsset>
 
 export function loadAssets(): Promise<void>{
@@ -18,7 +24,7 @@ export function loadAssets(): Promise<void>{
 }
 
 export function getTexture(textureName: string): Texture{
-    const texture = Loader.shared.resources[textureName].texture;
+    const texture = Loader.shared.resources[textureName]?.texture;
     if (texture){
         return texture;
     }
