cd341bf7672fd89d07f55a1b2c1abb219bb20dc8
Alex Heppell <alexheppell@protonmail.com>
2022-11-15 20:43:21 +0000

    remove gsap pixi

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import { loadAssets } from "./asset-loader";
 import { RoxApp } from "./components/application";
-import PixiPlugin from 'gsap/PixiPlugin';
-import * as PIXI from 'pixi.js-legacy';
-import gsap from 'gsap';
-
-// https://greensock.com/docs/v3/Plugins/PixiPlugin
-gsap.registerPlugin(PixiPlugin);
-PixiPlugin.registerPIXI(PIXI);
 
 loadAssets().then(() => {
     const app = new RoxApp();
     document.body.appendChild(app.view);
-})
+});
