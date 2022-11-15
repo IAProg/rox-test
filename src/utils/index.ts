7782e387c2d2d1a9e9348853eaf282fe80f7f209
Alex Heppell <alexheppell@protonmail.com>
2022-11-15 22:55:58 +0000

    generate random float values less than one

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,8 +15,6 @@ export const delay = (ms: number) => new Promise<void>(
 );
 
 export function randomFloat(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
     return (Math.random() * (max - min)) + min;
 }
 
